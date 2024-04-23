52cf686e64d231159e087fbf7f423b6209c4d2c7
Matheus <matheus.lunkes.corp@gmail.com>
2024-04-22 22:07:55 -0300

    Update Aula 1- Console.log

--- Seção 1 - Javascript basico para Iniciantes/aula1.js
+++ Seção 1 - Javascript basico para Iniciantes/aula1.js
@@ -1,1 +1,7 @@
-console.log("Ola Mundo");
+/*Aula 1 - Console.log*/
+
+// Para strings console.log pode receber com 'uma' ou "duas" aspas e `crases´
+
+console.log("Olá Mundo"); // Strings
+console.log('Matheus Felipe Lunkes'); // Strings
+console.log(10.65); // Numbers
