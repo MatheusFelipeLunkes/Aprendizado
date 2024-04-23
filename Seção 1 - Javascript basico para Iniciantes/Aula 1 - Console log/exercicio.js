cdd0c82c0fd0671a0fa4e988fd71847f9976e348
Matheus <matheus.lunkes.corp@gmail.com>
2024-04-22 22:12:32 -0300

    Exercicio referente a Aula 1 - Console.log

---
+++ Seção 1 - Javascript basico para Iniciantes/Aula 1 - Console log/exercicio.js
@@ -0,0 +1,5 @@
+/* Escreva a seguinte frase usando a funcao console.log:
+"Meu nome é "Nome". Estou aprendendo javascript às 10 da manhã"
+*/
+
+console.log("Meu nome é Matheus. Estou aprendendo javascript as 22 horas da noite.");
