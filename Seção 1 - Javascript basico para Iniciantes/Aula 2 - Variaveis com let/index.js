79eb4b9e0e7f002f05e444f2191d7b5ac4e5f896
Matheus <matheus.lunkes.corp@gmail.com>
2024-04-29 20:28:26 -0300

    Aula 2 - Variaveis com LET

---
+++ Seção 1 - Javascript basico para Iniciantes/Aula 2 - Variaveis com let/index.js
@@ -0,0 +1,17 @@
+// Oque são variaveis e para que servem? 
+/*
+Variaveis servem para "Salvar" determinado dado/atributo na memoria do computador.
+Exemplo:
+*/
+
+let nome = "Programer";
+let car = 'BMW';
+
+console.log(`I am ${nome}.`);
+console.log(car);
+// Para criar uma variavel com let, precisamos usar let e usar uma palavra que tenha significado para a referencia de valor da mesma como no exemplo acima, apos isso usamos o sinal de atribuição = para definir o valor desta variavel.
+
+//OBS: variaveis precisam ter nomes significativos, não podemos criar variaveis com palavras reservadas pela linguagem (let let = 'let';). Não podemos começar variaveis com numeros.
+//Sempre devemos usar a anotação camelCase.
+
+
