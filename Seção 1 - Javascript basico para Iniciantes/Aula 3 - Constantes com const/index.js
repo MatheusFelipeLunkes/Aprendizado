62532faf4ecd687f0764f09f48937558a831dc4b
Matheus <matheus.lunkes.corp@gmail.com>
2024-04-29 20:43:44 -0300

    Aula 3 - Variaveis com const

---
+++ Seção 1 - Javascript basico para Iniciantes/Aula 3 - Constantes com const/index.js
@@ -0,0 +1,15 @@
+// Oque são constantes?
+/*
+Constantes são variaveis cujo seus valores são imutaveis e indeclaraveis.
+EXEMPLO:
+*/
+
+const firstName = "Programer";
+const lastName = "Developer";
+
+console.log(`${firstName} || ${lastName}`);
+
+// Para criar uma variavel com const, precisamos usar const e usar uma palavra que tenha significado para a referencia de valor da mesma como no exemplo acima, apos isso usamos o sinal de atribuição = para definir o valor desta variavel.
+
+//OBS: variaveis precisam ter nomes significativos, não podemos criar variaveis com palavras reservadas pela linguagem. Não podemos começar variaveis com numeros.
+//Sempre devemos usar a anotação camelCase.
