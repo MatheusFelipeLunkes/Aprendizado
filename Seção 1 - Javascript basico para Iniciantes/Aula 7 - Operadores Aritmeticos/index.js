40104a982cce6230cd90449cf09d3433a07faf1b
Matheus <matheus.lunkes.corp@gmail.com>
2024-04-29 21:57:30 -0300

    Aula 7 - Operadores Aritméticos

---
+++ Seção 1 - Javascript basico para Iniciantes/Aula 7 - Operadores Aritmeticos/index.js
@@ -0,0 +1,47 @@
+// Aula 7 - Operadores Aritméticos
+
+/*
+Quais são os operadores aritmeticos?
+
+->      +   -> Adição / Concatenação
+->      -   -> Subtração
+->      *   -> Multiplicação
+->      /   -> Divisão
+->      %   -> Resto da divisão
+->      =   -> Atribuição
+->     **   -> Potenciação
+->     ==   -> Comparação  
+->     ++   -> Incremento
+->     --   -> Decremento
+*/
+
+let num1 = 1;
+let num2 = 2;
+let calc;
+
+calc = num1 + num2; // Adição / Atribuição
+console.log(calc);
+
+calc = num1 - num2; // Subtração
+console.log(calc);
+
+calc = num1 * num2; // Multiplicação
+console.log(calc);
+
+calc = num1 / num2; // Divisão
+console.log(calc);
+
+calc = num1 % num2; // Resto da divisão
+console.log(calc);
+
+calc = num1 ** num2; // Potenciação
+console.log(calc);
+
+calc = num1 == num2; // Comparação
+console.log(calc);
+
+num1++; // Incremento
+console.log(num1); 
+
+num1--; // Decremento
+console.log(num1); 
